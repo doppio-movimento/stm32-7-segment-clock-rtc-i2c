9b1e01d919d67320c151f386ffdd41917817d63d
Nolan Gagnon <109861807+doppio-movimento@users.noreply.github.com>
2026-05-24 15:04:17 +1200

    Create i2c.h

---
+++ include/i2c.h
@@ -0,0 +1,42 @@
+/**********************************************************************************\
+ * @file    I2C_PROJECT/include/i2c.h                                             *
+ * @author  Nolan R. H. Gagnon                                                    *
+ * @version V2.0                                                                  *
+ * @date    26-June-2017                                                          *
+ * @brief   I2C Interface.                                                        *
+ *                                                                                *
+ **********************************************************************************
+ * @attention                                                                     *
+ *                                                                                *
+ * <h2><center>&copy; COPYRIGHT(c) 2017 Nolan R. H. Gagnon </center></h2>         *
+ *                                                                                *
+\**********************************************************************************/
+#ifndef I2C_H
+#define I2C_H
+
+/**********************************************************************************\
+ *                                                                                *
+ *                              I2C CONSTANTS                                     *
+ *                                                                                *
+\**********************************************************************************/
+#define I2C_CLK_FREQ_DIV_2	((uint32_t) 1U << 28)
+
+/**********************************************************************************\
+ *                                                                                *
+ *                              I2C FUNCTIONS                                     *
+ *                                                                                *
+\**********************************************************************************/
+
+/* initialize i2c1 peripheral */
+void i2c1_init(void);
+
+/* configure pins used for i2c1 */
+static void i2c1_pins_init(void);
+
+/* transmit data on the i2c1 bus */
+void i2c1_transmit(uint8_t nbytes, uint8_t slvaddr, uint8_t *payload);
+
+/* read data from a slave on the i2c1 bus */
+void i2c1_read(uint8_t nbytes, uint8_t slvaddr, uint8_t *reg, uint8_t *storage_ptr);
+
+#endif
